579efc39ac8bd3a9655604c588a4811a86979cec
Tejaswini306 <saitejaswinij2002@gmail.com>
2023-06-23 17:23:56 +0530

    commited

--- Gaming-Merchant/client/src/components/TransactionHistory.jsx
+++ Gaming-Merchant/client/src/components/TransactionHistory.jsx
@@ -7,9 +7,9 @@ export default function TransactionHistory() {
         <input className="form-control me-2 w-75 bg-white text-dark" type="search" placeholder="Search in here..." aria-label="Search" />
         <button className="btn text-white bg-danger">Search</button>
       </div>
-      <ul>
-        <li className="transaction-item" >
-          <div className="d-flex align-items-center" style={{textDecoration:"none"}}>
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        <li className="transaction-item">
+          <div className="d-flex align-items-center">
             <img src="" alt="" className="transaction-image" />
             <div className="transaction-details">
               <p className="transaction-date mb-2">11.08.2022</p>
